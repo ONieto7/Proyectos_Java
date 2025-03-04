7b24aaf5cf7fe661d2e95f94511c9b3519d333fe
ONieto7 <oscarniro@gmail.com>
2025-03-04 13:27:46 +0100

    Añadidos más GET y POST

--- crud/crud/src/main/java/com/api/crud/controllers/UserController.java
+++ crud/crud/src/main/java/com/api/crud/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -23,4 +24,27 @@ public class UserController {
     public UserModel saveUser(@RequestBody UserModel user){
         return this.userService.saveUser(user);
     }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable ("id") Long id){
+        return this.userService.getById(id);
+    }
+
+    @PutMapping
+    public UserModel updateUserById(@RequestBody UserModel request, @PathVariable("id") Long id){
+        return this.userService.updateById(request, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteById(@PathVariable("id") Long id){
+        boolean ok = this.userService.deleteUser(id);
+
+        if(ok){
+            return "User with id " + id + "deleted";
+        }else {
+            return "Error, we have a problem ";
+        }
+    }
+
+
 }
