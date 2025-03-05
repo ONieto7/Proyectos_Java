8f7ba5d5718188d64800ee66732ba815a900c54e
ONieto7 <oscarniro@gmail.com>
2025-03-05 11:09:10 +0100

    Añadir página web inicial

--- crud/crud/src/main/java/com/api/crud/repositories/IUserRepository.java
+++ crud/crud/src/main/java/com/api/crud/repositories/IUserRepository.java
@@ -4,7 +4,6 @@ import com.api.crud.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 
 @Repository
 public interface IUserRepository extends JpaRepository<UserModel, Long> {
