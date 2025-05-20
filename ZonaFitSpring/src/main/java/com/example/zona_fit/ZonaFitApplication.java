e09836b261a1cddb72180d3c2e3b502d69978860
ONieto7 <oscarniro@gmail.com>
2025-05-20 19:38:06 +0200

    Refactor la aplicación ZonaFit: mejora de la estructura del código y adición de la lógica para gestionar clientes.

--- ZonaFitSpring/src/main/java/com/example/zona_fit/ZonaFitApplication.java
+++ ZonaFitSpring/src/main/java/com/example/zona_fit/ZonaFitApplication.java
@@ -1,13 +1,16 @@
-package com.example.zona_fit;
+package gm.zona_fit;
 
-import com.example.zona_fit.servicio.IClienteServicio;
-import org.slf4j.LoggerFactory;
+import gm.zona_fit.modelo.Cliente;
+import gm.zona_fit.servicio.IClienteServicio;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -17,16 +20,15 @@ public class ZonaFitApplication implements CommandLineRunner {
 	private IClienteServicio clienteServicio;
 
 	private static final Logger logger =
-            LoggerFactory.getLogger(ZonaFitApplication.class);
+			LoggerFactory.getLogger(ZonaFitApplication.class);
 
 	String nl = System.lineSeparator();
 
 	public static void main(String[] args) {
-		logger.info("Iniciando la aplicación");
-		//Levantar la fabrica de Spring
+		logger.info("Iniciando la aplicacion");
+		// Levantar la fabrica de spring
 		SpringApplication.run(ZonaFitApplication.class, args);
-		logger.info("Aplicación Finalizada");
-
+		logger.info("Aplicacion finalizada!");
 	}
 
 	@Override
@@ -34,27 +36,101 @@ public class ZonaFitApplication implements CommandLineRunner {
 		zonaFitApp();
 	}
 
-	private void zonaFitApp() {
+	private void zonaFitApp(){
 		var salir = false;
 		var consola = new Scanner(System.in);
-		while (!salir){
+		while(!salir){
 			var opcion = mostraMenu(consola);
-			//salir = ejecutarOciones(consola, opcion);
+			salir = ejecutarOpciones(consola, opcion);
 			logger.info(nl);
 		}
 	}
 
-	private int mostraMenu(Scanner consola) {
+	private int mostraMenu(Scanner consola){
 		logger.info("""
-				\n*** Aplicación Zona Fit ***
-					1.Listar Clientes
-					2.Buscar Cliente
-					3.Agregar Cliente
-					4.Modifcar Claiente
-					5.Eliminar Cliente
-					6.Salir
-				Elige una opcion:\s""");
-		var opcion = Integer.parseInt(consola.nextLine());
-		return opcion;
+        \n*** Aplicacion Zona Fit (GYM) ***
+		1. Listar Clientes
+		2. Buscar Cliente
+		3. Agregar Cliente
+		4. Modificar Cliente
+		5. Eliminar Cliente
+		6. Salir
+		Elige una opcion:\s""");
+		return Integer.parseInt(consola.nextLine());
+	}
+
+	private boolean ejecutarOpciones(Scanner consola, int opcion){
+		var salir = false;
+		switch (opcion){
+			case 1 -> {
+				logger.info(nl + "--- Listado de Clientes ---" + nl);
+				List<Cliente> clientes = clienteServicio.listarClientes();
+				clientes.forEach(cliente -> logger.info(cliente.toString() + nl));
+			}
+			case 2 -> {
+				logger.info(nl + "--- Buscar Cliente por Id ---" + nl);
+				logger.info("Id Cliente a buscar: ");
+				var idCliente = Integer.parseInt(consola.nextLine());
+				Cliente cliente = clienteServicio.buscarClientePorId(idCliente);
+				if(cliente != null)
+					logger.info("Cliente encontrado: " + cliente + nl);
+				else
+					logger.info("Cliente NO encontrado: " + cliente + nl);
+			}
+			case 3 -> {
+				logger.info("--- Agregar Cliente ---" + nl);
+				logger.info("Nombre: ");
+				var nombre = consola.nextLine();
+				logger.info("Apellido: ");
+				var apellido = consola.nextLine();
+				logger.info("Membresia: ");
+				var membresia = Integer.parseInt(consola.nextLine());
+				var cliente = new Cliente();
+				cliente.setNombre(nombre);
+				cliente.setApellido(apellido);
+				cliente.setMembresia(membresia);
+				clienteServicio.guardarCliente(cliente);
+				logger.info("Cliente agregado: " + cliente + nl);
+			}
+			case 4 -> {
+				logger.info("--- Modificar Cliente ---" + nl);
+				logger.info("Id Cliente: ");
+				var idCliente = Integer.parseInt(consola.nextLine());
+				Cliente cliente = clienteServicio.buscarClientePorId(idCliente);
+				if(cliente != null){
+					logger.info("Nombre: " );
+					var nombre = consola.nextLine();
+					logger.info("Apellido: ");
+					var apellido = consola.nextLine();
+					logger.info("Membresia: ");
+					var membresia = Integer.parseInt(consola.nextLine());
+					cliente.setNombre(nombre);
+					cliente.setApellido(apellido);
+					cliente.setMembresia(membresia);
+					clienteServicio.guardarCliente(cliente);
+					logger.info("Cliente modificado: " + cliente + nl);
+				}
+				else
+					logger.info("cliente NO encontrado: " + cliente + nl);
+			}
+			case 5 -> {
+				logger.info("--- Eliminar Cliente ---" + nl);
+				logger.info("Id Cliente: ");
+				var idCliente = Integer.parseInt(consola.nextLine());
+				var cliente = clienteServicio.buscarClientePorId(idCliente);
+				if(cliente != null){
+					clienteServicio.eliminarCliente(cliente);
+					logger.info("Cliente eliminado: " + cliente + nl);
+				}
+				else
+					logger.info("Cliente No encontrado: " + cliente + nl);
+			}
+			case 6 -> {
+				logger.info("Hasta pronto!" + nl + nl);
+				salir = true;
+			}
+			default -> logger.info("Opcion NO reconocida: " + opcion + nl);
+		}
+		return salir;
 	}
 }
