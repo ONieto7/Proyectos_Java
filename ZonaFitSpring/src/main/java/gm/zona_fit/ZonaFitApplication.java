0c444ad743746c3c1f38ff7ae94b2ffe5a3ea7eb
ONieto7 <oscarniro@gmail.com>
2025-06-04 12:45:06 +0200

    Actualizar dependencias de Spring Boot, agregar controlador y mejorar configuración de logging

--- ZonaFitSpring/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ ZonaFitSpring/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -2,11 +2,13 @@ package gm.zona_fit;
 
 import gm.zona_fit.modelo.Cliente;
 import gm.zona_fit.servicio.IClienteServicio;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
 import java.util.Scanner;
